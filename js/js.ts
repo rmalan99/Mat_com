8cfb586b41d7a46cba022dfe873698814a1fa738
rmalan99 <rmalan99@gmail.com>
2019-07-24 16:19:49 -0400

    generacion click+documentaccion

--- js/js.ts
+++ js/js.ts
@@ -1,12 +1,20 @@
 let option=document.body.getElementsByClassName("container")[1].getElementsByTagName("div");
-option[0].addEventListener("click",() => {let text:string=option[0].getElementsByTagName("a")[0].innerHTML;
-document.getElementById("title").innerHTML=text;
-let datos=document.getElementById("datos")
-let place_holder:Array<string>=["an1","an2"];
-for(var i=0;i<datos.childElementCount;i++){
-    datos.getElementsByTagName("input")[i].setAttribute("placeholder",place_holder[i]);
-}
-});
+
+
+option[0].addEventListener("click",);
+//observador de click generados ...v/
+//reduce codigo<<<<<<<<
+//crea funcion o interfaz independiente y pasa lista con parametros de placeholder
+
+// () => {
+// let text:string=option[0].getElementsByTagName("a")[0].innerHTML;    enlace al primer hijo y recuperacion de datos
+// document.getElementById("title").innerHTML=text;                     insercion de datos del primer hijo div
+// let datos=document.getElementById("datos")                           abrebiacion de elace padre datos colletor
+// let place_holder:Array<string>=["an1","an2"];                        tipos de datos array{contendora}
+// for(var i=0;i<datos.childElementCount;i++){                          inyeccion de propieda place holder
+//     datos.getElementsByTagName("input")[i].setAttribute("placeholder",place_holder[i]);
+// }
+// }
 // option[1].addEventListener("click",() => {alert("funciono")});
 // option[2].addEventListener("click",() => {alert("funciono")});
 // option[3].addEventListener("click",() => {alert("funciono")});
